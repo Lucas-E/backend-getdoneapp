beb8aff26ddb1592e546f1b466d8fe75f0238e24
Lucas-E <lucasgitacc@gmail.com>
2023-06-09 02:47:14 -0300

    put message that the connection was successfull

--- models/index.js
+++ models/index.js
@@ -12,8 +12,10 @@ const db = {};
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  console.log('Connected')
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
+  console.log('Connected')
 }
 
 fs
