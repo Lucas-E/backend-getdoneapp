4a1453630d4623c127ef475fcf55a5de1abe7ef3
Lucas-E <lucasgitacc@gmail.com>
2023-06-12 03:13:00 -0300

    Added authentication route

---
+++ routes/authentication.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models').User
+require('dotenv').config()
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+router.post('/login', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
