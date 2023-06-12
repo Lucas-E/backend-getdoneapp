400bf64124887b2095ca3d4dd50464311fdfb841
Lucas-E <lucasgitacc@gmail.com>
2023-06-12 03:54:05 -0300

    added login and logout routes

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,14 +1,58 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models').User
-require('dotenv').config()
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const User = require("../models").User;
+require("dotenv").config();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-router.post('/login', async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-})
+router.post("/login", async (req, res) => {
+	try {
+		const { email, password } = req.body;
+		const user = await User.findOne({
+			where: {
+				email: email,
+			},
+		});
+		const userPassword = user.password;
+		const comparation = await bcrypt.compare(password, userPassword);
+		if (comparation) {
+			const refreshToken = jwt.sign(
+				{ id: user.id, email: user.email },
+				process.env.REFRESH_SECRET,
+				{
+					expiresIn: "1m",
+				}
+			);
+			const accessToken = jwt.sign(
+				{ id: user.id, email: user.email },
+				process.env.ACESS_TOKEN,
+				{
+					expiresIn: "30s",
+				}
+			);
+			const updatedUser = await User.update(
+				{
+					token: refreshToken,
+				},
+				{
+					where: {
+						email: email,
+					},
+				}
+			);
+            res.cookie('refreshToken', refreshToken, {
+                maxAge: 60,
+                httpOnly: true
+            })
+            return res.status(200).json(accessToken)
+		} else {
+			return res.sendStatus(400).json({
+				message: "Wrong Password",
+			});
+		}
+	} catch (error) {
+		return res.status(400).json({
+			message: "Error while logging in",
+		});
+	}
+});
