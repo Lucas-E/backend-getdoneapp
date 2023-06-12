1a90fa70b5e5bf557237d96162ee82113c754fc7
Lucas-E <lucasgitacc@gmail.com>
2023-06-12 02:15:39 -0300

    added hasMany relation

--- models/user.js
+++ models/user.js
@@ -1,27 +1,46 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  User.init({
-    username: DataTypes.STRING,
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    token: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
+	class User extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+      this.hasMany(Model.Task, {foreignKey:'userId'})
+		}
+	}
+	User.init(
+		{
+			username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique:true
+      },
+			name: {
+        type:DataTypes.STRING,
+        allowNull:false
+      },
+			email: {
+        type: DataTypes.STRING,
+        allowNull:false,
+        unique: true
+      },
+			password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+			token: {
+				type: DataTypes.STRING,
+				allowNull: true,
+			},
+		},
+		{
+			sequelize,
+			modelName: "User",
+		}
+	);
+	return User;
 };
