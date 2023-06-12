a1cad44a090423764700c75498331d77c5e118e2
Lucas-E <lucasgitacc@gmail.com>
2023-06-12 16:03:19 -0300

    added try catch

--- middleware/checkJwt.js
+++ middleware/checkJwt.js
@@ -1,29 +1,39 @@
 const jwt = require("jsonwebtoken");
-require('dotenv').config()
+require("dotenv").config();
 
 const checkUserAuthentication = async (req, res, next) => {
-	const authHeader = req.headers.authorization;
-	if (!authHeader) {
-		return res.status(400).json({
-			message: "Access token not informed",
+	try {
+		const authHeader = req.headers.authorization;
+		if (!authHeader) {
+			return res.status(400).json({
+				message: "Access token not informed",
+			});
+		}
+		const accessToken = authHeader.split(" ")[1];
+		if (!accessToken) {
+			return res.status(400).json({
+				message: "Access token not informed",
+			});
+		}
+		const checking = jwt.verify(
+			accessToken,
+			process.env.ACCESS_SECRET,
+			(err, decoded) => {
+				if (err) {
+					return res.status(401).json({
+						message: "Expired access token",
+					});
+				}
+				const user = decoded.user;
+				req.user = user;
+				next();
+			}
+		);
+	} catch (error) {
+		return res.status(401).json({
+			message: "Unauthorized",
 		});
 	}
-	const accessToken = authHeader.split(" ")[1];
-	if (!accessToken) {
-		return res.status(400).json({
-			message: "Access token not informed",
-		});
-	}
-    const checking = jwt.verify(accessToken, process.env.ACCESS_SECRET, (err, decoded) => {
-        if(err){
-            return res.status(401).json({
-                message: "Expired access token"
-            })
-        }
-        const user = decoded.user
-        req.user = user
-        next()
-    })
 };
 
-module.exports = checkUserAuthentication
+module.exports = checkUserAuthentication;
