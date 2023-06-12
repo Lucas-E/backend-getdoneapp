7708add85c7f864b51ffe7676dcddab98aa4f9ab
Lucas-E <lucasgitacc@gmail.com>
2023-06-12 01:38:37 -0300

    removed script

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const Sequelize = require('./models')
-
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const Sequelize = require("./models");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
+app.use(express.static(path.join(__dirname, "public")));
 
 module.exports = app;
