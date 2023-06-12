c1e14498dac2e534e83b525b721570b3dc8173f8
Lucas-E <lucasgitacc@gmail.com>
2023-06-12 02:06:07 -0300

    added unique to email field

--- migrations/20230612044012-create-user.js
+++ migrations/20230612044012-create-user.js
@@ -22,7 +22,8 @@ module.exports = {
       },
       email: {
         type: Sequelize.STRING,
-        allowNull:false
+        allowNull:false,
+        unique: true
       },
       password: {
         type: Sequelize.STRING,
