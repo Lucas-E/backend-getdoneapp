6c5e219c8e1521f480fcacf14eba7ab4dadbc192
Lucas-E <lucasgitacc@gmail.com>
2023-06-12 02:17:29 -0300

    changed relations

--- models/task.js
+++ models/task.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.belongsTo(Model.User, {foreignKey:'userId'})
+      this.belongsTo(models.User, {foreignKey:'userId'})
     }
   }
   Task.init({
--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
 		 */
 		static associate(models) {
 			// define association here
-      this.hasMany(Model.Task, {foreignKey:'userId'})
+      this.hasMany(models.Task, {foreignKey:'userId'})
 		}
 	}
 	User.init(
