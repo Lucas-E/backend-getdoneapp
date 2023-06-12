83b1daed831b325b00f5f17bd1f64399dcb08c43
Lucas-E <lucasgitacc@gmail.com>
2023-06-12 02:45:20 -0300

    added route to create user and update password or username in case of it being forgotten

---
+++ routes/registration.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const User = require("../models").User;
+
+require("dotenv").config();
+
+router.post("/", async (req, res) => {
+	try {
+		const { username, name, password, email } = req.body;
+		const salt = await bcrypt.salt(process.env.SALT);
+		const hashedPassword = await bcrypt.hash(password, salt);
+		const createdUser = await User.create({
+			username: username,
+			name: name,
+			password: hashedPassword,
+			email: email,
+		});
+		return res.sendStatus(200);
+	} catch (error) {
+		return res.status(400).json({ message: "Error while singing up user" });
+	}
+});
+
+router.patch("/password", async (req, res) => {
+	try {
+		const { password, email } = req.body;
+		const salt = await bcrypt.salt(process.env.SALT);
+		const hashedPassword = await bcrypt.hash(password, salt);
+		const updatedUser = await User.update(
+			{ password: hashedPassword },
+			{
+				where: { email: email },
+			}
+		);
+		return res.sendStatus(200);
+	} catch (error) {
+		return res.status(400).json({
+			message: "Error while trying to update user password",
+		});
+	}
+});
+
+router.patch("/username", async (req, res) => {
+	try {
+		const { username, email } = req.body;
+		const updatedUser = await User.update(
+			{ username: username },
+			{
+				where: { email: email },
+			}
+		);
+		return res.sendStatus(200);
+	} catch (error) {
+		return res.status(400).json({
+			message: "Error while trying to update user password",
+		});
+	}
+});
+
+module.exports = router;
